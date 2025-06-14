001b8aa656212508f41e29de5011c76c2ea623fa
192211028 <123543116+192211028@users.noreply.github.com>
2025-06-14 18:43:38 +0530

    DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "surya@VOC";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
